bad4756fd69c327784a853a07656fe214bff6ac6
tcper <tcper@163.com>
2020-03-27 15:09:43 +0800

    update assets

---
+++ assets/precache-manifest.e9af5042549a76dbce75d75f9cee72b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2a6a5f3524d252d6f1a94cd4e7bac71",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7376f5c53e19d0540878",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9008320be1f4d5893799",
+    "url": "/static/js/2.fac0c9fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fac0c9fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7376f5c53e19d0540878",
+    "url": "/static/js/main.7b1a6eaf.chunk.js"
+  },
+  {
+    "revision": "65b65c71da0907c8fa1f",
+    "url": "/static/js/runtime-main.35559588.js"
+  }
+]);
